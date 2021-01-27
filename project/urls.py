422ab1a9e26385220681499f16e5d51e043df82b
328905689@qq.com <328905689@qq.com>
2021-01-28 00:20:16 +0800

    登录页面

--- project/urls.py
+++ project/urls.py
@@ -9,12 +9,12 @@ from project import views
 app_name = 'project'
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('lists/',views.lists, name='lists'),
-    path('query/',views.project_query, name='query'),
-    path('del/',views.project_del, name='del'),
-    path('save/',views.project_save, name='save'),
+    path('index', views.index, name='index'),
+    path('lists/', views.lists, name='lists'),
+    path('query/', views.project_query, name='query'),
+    path('del/', views.project_del, name='del'),
+    path('save/', views.project_save, name='save'),
     
-    path('test/',views.test, name='test'),
+    path('test/', views.test, name='test'),
     path('data/', views.datatable, name='data'),
     ]
